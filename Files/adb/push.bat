739ed868fcd5a13e3dc65ee072140825180ae86e
AridaneAM <40564663+AridaneAM@users.noreply.github.com>
2019-02-17 14:11:40 +0100

    Add complementary files

---
+++ Files/adb/push.bat
@@ -0,0 +1,3 @@
+.\adb push magisk\Magisk-v15.3.zip /sdcard/Root_MiA1
+.\adb push magisk\magisk-permissive-script_v1.1.zip /sdcard/Root_MiA1/modules
+.\adb push magisk\XiaomiMiA1Fixesv5Oreo.zip /sdcard/Root_MiA1/modules
